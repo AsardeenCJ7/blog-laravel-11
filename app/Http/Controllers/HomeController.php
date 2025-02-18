159ba245a7cf92a089b81a58834bc0ef72613b20
M.A. Asardeen <asardeenbse@gmail.com>
2025-02-18 11:56:38 +0530

    get data from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,28 +10,31 @@ class HomeController extends Controller
     //
 
     public function detail($id){
-        $posts = $this->getPost();
-        $post = collect($posts)->firstWhere('id',$id);
+        // $posts = $this->getPost();
+       
+        // $post = collect($posts)->firstWhere('id',$id);
+        $post =Post::find($id); // Using pass arguments primary key 
         return view('detail', compact('post'));
     }
 
 public function index(){
-    $title = 'Blog Cj7';
+    $title = 'Blog';
     // $posts = $this->getPost();
-    $posts= Post::all();
+    $posts= Post::all();  // from database
     return view('index',compact('title','posts'));
 }
 
-private function getPost(){
-    $data = [
-        ['id'=>'1','title'=>'post 1 Edit', 'content'=> 'content of post 1 Edit database not included'],
-        ['id'=>'2','title'=>'post 2', 'content'=> 'content of post 2 db'],
-    ];
-    // Here we can convert array to object their 3 methods one of this methods called json_decode
-    // Convert an Array to an Object in Laravel (PHP)
-    $objects = json_decode(json_encode($data));
-    return $objects;
-}
+// private function getPost(){
+//     //get post by id value from database
+//     $data = [
+//         ['id'=>'1','title'=>'post 1 Edit', 'content'=> 'content of post 1 Edit database not included'],
+//         ['id'=>'2','title'=>'post 2', 'content'=> 'content of post 2 db'],
+//     ];
+//     // Here we can convert array to object their 3 methods one of this methods called json_decode
+//     // Convert an Array to an Object in Laravel (PHP)
+//     $objects = json_decode(json_encode($data));
+//     return $objects;
+// }
 
 
 
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,9 +5,10 @@
     <div class="row">
         <div class="col-lg-8">
             <h1 class="mb-4">{{$post->title}}</h1>
-            <p class="text-muted">Posted on January 1, 2024</p>
-            <img src="https://via.placeholder.com/800x400" class="img-fluid mb-4" alt="Blog Image">
-            <!-- <p>{{Str::limit($post->content,80)}}</p> -->
+            <p class="text-muted">Posted On {{$post->created_at->format('M D, Y')}}</p>
+            <!-- Time stamp value dd($post->created_at) -->
+            <img src="{{$post->img_url}}" class="img-fluid mb-4" alt="Blog Image">
+            <p>{{$post->content}}</p>
 
         </div>
         <div class="col-lg-4">
